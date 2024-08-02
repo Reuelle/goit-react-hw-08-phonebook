da2782493b02b766825aa0ddfa332f9a4bc170b7
Ruel Paya <ruel.paya@gmail.com>
2024-08-02 23:21:47 +0800

    Update operation.js

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -1,40 +1,93 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'https://65f11210da8c6584131ccbf1.mockapi.io/contacts';
+const API_URL = 'https://connections-api.goit.global/';
 
+// Utility to set authorization token
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+// Register new user
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/signup`, credentials);
+      setAuthToken(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Login user
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/login`, credentials);
+      setAuthToken(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Logout user
+export const logoutUser = createAsyncThunk(
+  'auth/logoutUser',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post(`${API_URL}/users/logout`);
+      setAuthToken(null);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Fetch contacts
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
-  async (_, { rejectWithValue }) => {
+  async (_, thunkAPI) => {
     try {
-      const response = await axios.get(API_URL);
+      const response = await axios.get(`${API_URL}/contacts`);
       return response.data;
     } catch (error) {
-      return rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.response.data);
     }
   }
 );
 
+// Add a new contact
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (newContact, { rejectWithValue }) => {
+  async (contactData, thunkAPI) => {
     try {
-      const response = await axios.post(API_URL, newContact);
+      const response = await axios.post(`${API_URL}/contacts`, contactData);
       return response.data;
     } catch (error) {
-      return rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.response.data);
     }
   }
 );
 
+// Delete a contact
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (contactId, { rejectWithValue }) => {
+  async (contactId, thunkAPI) => {
     try {
-      await axios.delete(`${API_URL}/${contactId}`);
+      await axios.delete(`${API_URL}/contacts/${contactId}`);
       return contactId;
     } catch (error) {
-      return rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.response.data);
     }
   }
 );
