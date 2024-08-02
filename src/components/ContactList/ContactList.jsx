d899900aed034fd1ad3a66525c46066844bfb3f8
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-02 10:14:48 +0800

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,29 @@
+import css from './ContactList.module.css';
+import { useSelector } from 'react-redux';
+import { selectContacts, selectFilter } from 'redux/selector';
+import { Contact } from 'components/Contact/Contact';
 
+export const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+
+  if (!contacts) {
+    return null;
+  }
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div className={css.wraperContactList}>
+      <ul className={css.contactList}>
+        {visibleContacts.map((contact) => (
+          <li key={contact.id} className={css.contactListItem}>
+            <Contact contact={contact} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
