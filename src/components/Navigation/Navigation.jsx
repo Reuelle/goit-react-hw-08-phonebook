bdb4a227890accd96e07eee69ca17ab34cb621b3
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-04 10:29:24 +0800

    Update Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { useAuth } from '../hooks';
+import { Links } from './Navigation.css.module.css';
+
+const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav>
+      <Links to="/" end>
+        Home
+      </Links>
+      {isLoggedIn && <Links to="/contacts">Phonebook</Links>}
+    </nav>
+  );
+};
+
+export default Navigation;
