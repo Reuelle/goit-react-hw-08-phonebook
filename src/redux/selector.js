b7f21987af71d148f0334344b0574507f1668968
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-04 13:40:46 +0800

    Update selector.js

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,7 +1,15 @@
-export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectUser = state => state.auth.user;
+export const selectContacts = state => state.contacts.items;
+export const selectFilter = state => state.filter;
+export const selectError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.isLoading;
 
-export const selectEmail = state => state.auth.email;
-
-export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
