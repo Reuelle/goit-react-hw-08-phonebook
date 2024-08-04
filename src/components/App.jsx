73a09b095c154175fcfb923cc2ab9a3b402c41df
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-04 10:58:41 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import Register from '../pages/Register/Register'; // Correct import
 import Login from '../pages/Login/Login';
 import Contacts from '../pages/Contacts/Contacts';
-import Navigation from '../Navigation/Navigation';
+import Navigation from './Navigation/Navigation';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
