3aa5e25d8927763172a246e5f4b8387cb7c26672
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-04 10:30:50 +0800

    Update AuthNavi.jsx

--- src/components/AuthNavi/AuthNavi.jsx
+++ src/components/AuthNavi/AuthNavi.jsx
@@ -1,1 +1,10 @@
+import { Links } from './AuthNavi.module.css';
 
+export default function AuthNav() {
+  return (
+    <div>
+      <Links to="/registration">Registration</Links>
+      <Links to="/login">Login</Links>
+    </div>
+  );
+}
