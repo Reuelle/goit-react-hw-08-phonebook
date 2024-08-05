64a6beb424b0a1f4592a20f674367d4a58dcf21c
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-05 11:14:21 +0800

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { logoutUser } from '../../redux/slice/authSlice';
+import { logoutUser } from '../../redux/slice/auth-slice';
 import css from './UserMenu.module.css';
 
 const UserMenu = () => {
