56a1e37937aa0781f58a3b96aaa5a7fcae7017f8
Reuelle <134071413+Reuelle@users.noreply.github.com>
2024-08-06 16:30:01 +0000

    Deploying to gh-pages from @ Reuelle/goit-react-hw-08-phonebook@6da30184ee64be298707bdc3fa7aebf8aeb3bcd8 🚀

---
+++ static/js/544.07e995cc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,n){n.r(t);var i=n(3329),c={container:{minHeight:"calc(50vh - 50px)",minWidth:"calc(50vw - 50px)",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"},title:{fontWeight:700,fontSize:50,textAlign:"center",color:"#1677ff"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Phonebook"})})}}}]);
+//# sourceMappingURL=544.07e995cc.chunk.js.map
