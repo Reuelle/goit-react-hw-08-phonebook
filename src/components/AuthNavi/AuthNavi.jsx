98eb512b5ab72ba8d37806e8350fc32788478b18
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-06 14:41:47 +0800

    Update AuthNavi.jsx

--- src/components/AuthNavi/AuthNavi.jsx
+++ src/components/AuthNavi/AuthNavi.jsx
@@ -1,10 +1,17 @@
-import { Links } from './AuthNavi.module.css';
+// AuthNavi.jsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './AuthNavi.module.css';
 
 export default function AuthNav() {
   return (
     <div>
-      <Links to="/registration">Registration</Links>
-      <Links to="/login">Login</Links>
+      <NavLink to="/registration" className={styles.link} activeClassName={styles.active}>
+        Registration
+      </NavLink>
+      <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
+        Login
+      </NavLink>
     </div>
   );
 }
