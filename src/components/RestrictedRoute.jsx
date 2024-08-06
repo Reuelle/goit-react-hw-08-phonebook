1f98b8c64a83b48b1781210c5a34484c1c2e2ff0
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-06 12:46:35 +0800

    Update RestrictedRoute.jsx

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,1 +1,10 @@
 
+import { Navigate } from 'react-router-dom';
+
+import { useAuth } from './hooks';
+
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  // component - RegisterPage or LoginPage
+  const { isLoggedIn } = useAuth(); // isLoggedIn - true or false
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component; // if isLoggedIn is true, then redirect to redirectTo, else render Component
+};
