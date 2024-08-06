45b6658396bf15ef229fec9d8b52e43ca99490a7
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-06 09:08:10 +0800

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,11 @@
-import css from './Filter.module.css';
-import { useDispatch } from 'react-redux';
+import React from 'react'; // Add this if not already present
+import { useDispatch, useSelector } from 'react-redux'; // Import useSelector here
 import { setFilter } from 'redux/slice/filter';
 import { FcSearch } from 'react-icons/fc';
-import { selectContacts, selectFilter } from 'redux/selectors';
+// Import Title and InputStyles if they are custom components
+// import Title from 'path/to/Title';
+// import InputStyles from 'path/to/InputStyles'; 
 
-  
 function Filter() {
   const dispatch = useDispatch();
   const filter = useSelector(selectFilter);
@@ -14,15 +15,14 @@ function Filter() {
   };
 
   return useSelector(selectContacts).length < 1 ? (
-    <Title>Add your first contact</Title>
+    <h2>Add your first contact</h2> // Use h2 or another appropriate HTML tag if Title is not a component
   ) : (
-    <InputStyles
+    <input // Use standard input if InputStyles is not a component
       type="text"
       name="filter"
       placeholder="Search by name"
       value={filter}
       onChange={handleFilterChange}
-      prefix={<FcSearch />}
     />
   );
 }
