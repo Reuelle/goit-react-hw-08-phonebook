a12bddd242dc877317ad3f71617d9f54dd0e6f39
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-07 10:48:38 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,39 +11,46 @@ export const ContactForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
-    const name = form.elements.name.value;
-    const number = form.elements.number.value;
+    const name = form.elements.name.value.trim();
+    const number = form.elements.number.value.trim();
     form.reset();
-    
+
+    // Check for duplicate contact names
     if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return;
     }
+
+    // Dispatch action to add new contact
     dispatch(addContact({ name, number }));
   };
 
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.formLabel}>Name </label>
-      <input
-        className={css.formName}
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces."
-        required
-        placeholder="Enter name"
-      />
-      <label className={css.formLabel}>Number </label>
-      <input
-        className={css.formNumber}
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +."
-        required
-        placeholder="Enter phone number"
-      />
+      <label className={css.formLabel}>
+        Name
+        <input
+          className={css.formName}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash, and spaces."
+          required
+          placeholder="Enter name"
+        />
+      </label>
+      <label className={css.formLabel}>
+        Number
+        <input
+          className={css.formNumber}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses, and can start with +."
+          required
+          placeholder="Enter phone number"
+        />
+      </label>
       <button className={css.formBtn} type="submit">
         Add contact
       </button>
