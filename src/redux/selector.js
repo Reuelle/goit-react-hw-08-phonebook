c1b49abd11020faf56316ea41e66add708244db9
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-08 11:26:54 +0800

    Update selector.js

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,7 +1,9 @@
-// redux/selector.js
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts;
+export const selectContacts = state => state.contacts.items;
+export const selectFilter = state => state.filter;
+export const selectError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
