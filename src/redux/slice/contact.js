7d6e0030f964bc3a3937de1f92dd9cdb5726b95a
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-08 10:27:45 +0800

    Update contact.js

--- src/redux/slice/contact.js
+++ src/redux/slice/contact.js
@@ -1,17 +1,17 @@
+// redux/slice/contact.js
 import { createSlice } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid'; // Install uuid package if you haven't
+import { v4 as uuidv4 } from 'uuid';
 
 const contactSlice = createSlice({
   name: 'contacts',
   initialState: [],
   reducers: {
     addContact: (state, action) => {
-      state.push({ ...action.payload, id: uuidv4() }); // Add unique ID
+      state.push({ ...action.payload, id: uuidv4() });
     },
     deleteContact: (state, action) => {
       return state.filter(contact => contact.id !== action.payload);
     },
-    // Other reducers...
   },
 });
 
