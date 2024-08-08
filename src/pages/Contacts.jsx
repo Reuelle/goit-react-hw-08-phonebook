ccc330716d42a8e168d88e5468254505adb9dd1f
Ruel Paya <ruel.paya@gmail.com>
2024-08-08 20:37:24 +0800

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,1 +1,25 @@
+// src/pages/Contacts/Contacts.jsx
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import ContactForm from '../../components/ContactForm/ContactForm'; // Default import
+import ContactList from '../../components/ContactList/ContactList';
+import Filter from '../../components/Filter/Filter';
+
+function Contacts() {
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Phonebook📱</title>
+        </Helmet>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </HelmetProvider>
+    </>
+  );
+}
+
+export default Contacts;
 
