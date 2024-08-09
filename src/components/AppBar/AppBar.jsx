85d582d8d03ca9b1c6223e505edaac7ec72d8aeb
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-09 16:17:45 +0800

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import UserMenu from '../UserMenu/UserMenu';
 import AuthNav from '../AuthNavi/AuthNavi';
-import styles from './AppBar.module.css'; // Import the CSS module
+import styles from './AppBar.module.css'; 
 import { useAuth } from '../hooks';
 
 const AppBar = () => {
