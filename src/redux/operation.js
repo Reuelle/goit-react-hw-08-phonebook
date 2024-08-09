88fdbf7decbe5ecbe3d26cbac3d37ab6f614915b
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-09 14:40:00 +0800

    Update operation.js

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -11,7 +11,6 @@ export const fetchContacts = createAsyncThunk(
       localStorage.setItem('contacts', JSON.stringify(response.data));
       return response.data;
     } catch (error) {
-      const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
       return rejectWithValue(error.message);
     }
   }
