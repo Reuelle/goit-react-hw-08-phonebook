bbef9cd33ef792ccb48be8971168389a9d0f629a
Ruel Paya <ruel.paya@gmail.com>
2024-08-10 06:09:52 +0800

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,21 @@
-import { ProgressBar } from 'react-loader-spinner';
+import { ColorRing } from 'react-loader-spinner';
 
 export const Loader = () => (
-  <ProgressBar
-    height="80"
-    width="80"
-    ariaLabel="progress-bar-loading"
-    wrapperStyle={{ display: 'flex', width: '100%', justifyContent: 'center' }}
-    wrapperClass="progress-bar-wrapper"
-    borderColor="#100c08"
-    barColor="#e0e0e0"
-  />
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      marginTop:'70px',
+      position:'relative',
+    }}
+  >
+    <ColorRing
+      visible={true}
+      height="70"  
+      width="70"   
+      colors={['#9d9dff', 'bab1ff', '#c4c4ff', '#d8d8ff', '#ebebff']}
+      ariaLabel="color-ring-loading"
+    />
+  </div>
 );
