d1fe0c1e54a77f433ee40e30c87512dc390cad9d
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-09 10:55:52 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,40 +1,52 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addContact } from '../../redux/operation';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/slice/contact';
+import { selectContacts } from 'redux/selector';
+import css from './ContactForm.module.css';
 
-const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+export const ContactForm = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
+    const form = e.target;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+    form.reset();
+    
+    if (contacts.find(contact => contact.name === name)) {
+      alert(${name} is already in contacts);
+      return;
+    }
     dispatch(addContact({ name, number }));
-    setName('');
-    setNumber('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Input Name
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label>
-        Input Number
-        <input
-          type="tel"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-        />
-      </label>
-      <button type="submit">Add contact</button>
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <label className={css.formLabel}>Name </label>
+      <input
+        className={css.formName}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces."
+        required
+        placeholder="Enter name"
+      />
+      <label className={css.formLabel}>Number </label>
+      <input
+        className={css.formNumber}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +."
+        required
+        placeholder="Enter phone number"
+      />
+      <button className={css.formBtn} type="submit">
+        Add contact
+      </button>
     </form>
   );
-};
-
-export default ContactForm;
+};contactform.jsx
