483260d1354236457d253f75e9d8ca4cde278ef2
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-23 20:28:49 +0800

    Update Auth-operations.js

--- src/redux/Auth/Auth-operations.js
+++ src/redux/Auth/Auth-operations.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 // Set the base URL for axios requests
-axios.defaults.baseURL = 'https://connections-api.goit.global/';
+axios.defaults.baseURL = 'https://expense-tracker.b.goit.study/api/';
 
 
 // Function to set the Authorization header for axios requests
